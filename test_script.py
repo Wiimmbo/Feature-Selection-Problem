154e4b9f9e64fc350ba4b4b58cc13eb200afec50
pavsmg <152363561+pavsmg@users.noreply.github.com>
2024-07-01 15:29:36 -0600

    test_script.py
    Seed combination added

--- test_script.py
+++ test_script.py
@@ -16,77 +16,86 @@ def test_realDE():
     encoders = ['label', 'target', 'drop']
     loss_functions = ['r2', 'rmse', 'combined']
     regression_models = ['ridge', 'lasso', 'linear']
+    seeds = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]  # 10 primeros números primos
     
     data_path = ROOT_DIR + '/data/listings.csv'
     
     for encoder in encoders:
         for regression_model in regression_models:
             for loss_function in loss_functions:
-                print(Fore.YELLOW + f'\n\nExperiment: Encoder: {encoder} - Regression Model: {regression_model} - Loss Function: {loss_function}')
-                #Set the evaluator
-                evaluator = Evaluator(data_path, encoder, regression_model, loss_function)
-                
-                print(Fore.CYAN + 'All features Loss:', evaluator.evaluate([1]*len(evaluator.features)))
-                
-                #Set the differential evolution
-                de = RealDifferentialEvolution(evaluator)
-                if loss_function == 'rmse':
-                    action = 'minimize'
-                else:
-                    action = 'maximize'
+                for seed in seeds:
+                    np.random.seed(seed)
+                    print(Fore.YELLOW + f'\n\nExperiment: Encoder: {encoder} - Regression Model: {regression_model} - Loss Function: {loss_function} - Seed: {seed}')
+                    #Set the evaluator
+                    evaluator = Evaluator(data_path, encoder, regression_model, loss_function)
                     
-                #Optimize
-                de.optimize(5, 200, 0.1, 0.1, 0.4, action)
+                    print(Fore.CYAN + 'All features Loss:', evaluator.evaluate([1]*len(evaluator.features)))
+                    
+                    #Set the differential evolution
+                    de = RealDifferentialEvolution(evaluator)
+                    if loss_function == 'rmse':
+                        action = 'minimize'
+                    else:
+                        action = 'maximize'
+                        
+                    #Optimize
+                    de.optimize(5, 200, 0.1, 0.1, 0.4, action)
 
 def test_combDE():
     encoders = ['label', 'target', 'drop']
     loss_functions = ['r2', 'rmse', 'combined']
     regression_models = ['ridge', 'lasso', 'linear']
+    seeds = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]  # 10 primeros números primos
     
     data_path = ROOT_DIR + '/data/listings.csv'
     
     for encoder in encoders:
         for regression_model in regression_models:
             for loss_function in loss_functions:
-                print(Fore.YELLOW + f'\n\nExperiment: Encoder: {encoder} - Regression Model: {regression_model} - Loss Function: {loss_function}')
-                #Set the evaluator
-                evaluator = Evaluator(data_path, encoder, regression_model, loss_function)
-                
-                print(Fore.CYAN + 'All features Loss:', evaluator.evaluate([1]*len(evaluator.features)))
-                
-                #Set the differential evolution
-                de = ComibnatorialDifferentialEvolution(evaluator)
-                if loss_function == 'rmse':
-                    action = 'minimize'
-                else:
-                    action = 'maximize'
+                for seed in seeds:
+                    np.random.seed(seed)
+                    print(Fore.YELLOW + f'\n\nExperiment: Encoder: {encoder} - Regression Model: {regression_model} - Loss Function: {loss_function} - Seed: {seed}')
+                    #Set the evaluator
+                    evaluator = Evaluator(data_path, encoder, regression_model, loss_function)
                     
-                #Optimize
-                de.optimize(5, 200, 0.1, 0.1, action)
+                    print(Fore.CYAN + 'All features Loss:', evaluator.evaluate([1]*len(evaluator.features)))
+                    
+                    #Set the differential evolution
+                    de = ComibnatorialDifferentialEvolution(evaluator)
+                    if loss_function == 'rmse':
+                        action = 'minimize'
+                    else:
+                        action = 'maximize'
+                        
+                    #Optimize
+                    de.optimize(5, 200, 0.1, 0.1, action)
 
 def test_PSO():
     encoders = ['label', 'target', 'drop']
     loss_functions = ['r2', 'rmse', 'combined']
     regression_models = ['ridge', 'lasso', 'linear']
+    seeds = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]  # 10 primeros números primos
     
     data_path = ROOT_DIR + '/data/listings.csv'
     
     for encoder in encoders:
         for regression_model in regression_models:
             for loss_function in loss_functions:
-                print(Fore.YELLOW + f'\n\nExperiment: Encoder: {encoder} - Regression Model: {regression_model} - Loss Function: {loss_function}')
-                #Set the evaluator
-                evaluator = Evaluator(data_path, encoder, regression_model, loss_function)
-                
-                print(Fore.CYAN + 'All features Loss:', evaluator.evaluate([1]*len(evaluator.features)))
-                
-                #Set the differential evolution
-                pso = PSO(evaluator)
-                
-                if loss_function == 'rmse':
-                    pso.minimize(50, 15, 0, 0.9, 0.1, -np.inf)
-                else:
-                    pso.maximize(50, 15, 0, 0.9, 0.1, np.inf)
+                for seed in seeds:
+                    np.random.seed(seed)
+                    print(Fore.YELLOW + f'\n\nExperiment: Encoder: {encoder} - Regression Model: {regression_model} - Loss Function: {loss_function} - Seed: {seed}')
+                    #Set the evaluator
+                    evaluator = Evaluator(data_path, encoder, regression_model, loss_function)
+                    
+                    print(Fore.CYAN + 'All features Loss:', evaluator.evaluate([1]*len(evaluator.features)))
+                    
+                    #Set the PSO
+                    pso = PSO(evaluator)
+                    
+                    if loss_function == 'rmse':
+                        pso.minimize(50, 15, 0, 0.9, 0.1, -np.inf)
+                    else:
+                        pso.maximize(50, 15, 0, 0.9, 0.1, np.inf)
 
 
 # %%
