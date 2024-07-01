886bb45b740b214c64700c4ae4f7c5d66a928741
Wiimmbo <105682037+Wiimmbo@users.noreply.github.com>
2024-06-30 18:24:36 -0600

    Loss function implemented, combined loss added

--- LossFunction.py
+++ LossFunction.py
@@ -1,3 +1,4 @@
+#%%
 """
 Loss Function Module
 ====================
@@ -5,58 +6,136 @@ This module contains the loss function class for the FSP optimization problem.
 """
 #Libraries
 import pandas as pd
+
+#Regression models
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LinearRegression
+
+#Data split
+from sklearn.model_selection import train_test_split
+
+import os
+#Root path
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+#%%
 class Evaluator:
     """
     Class for evaluating the subset of features.
     """
-    def __init__(self, data, loss_function: str = 'r2'):
+    def __init__(self, data_path: str, regression_model:str , loss_function: str) -> None:
         """
         Constructor for the loss function
         
         Parameters
         ----------
-        data : pandas.DataFrame
-            The data for the loss function.
+        data : str
+            Path to the csv file containing the data.
+        regression_model : str
+            The regression model to fit.
+        loss_function : str
+            The loss function to use.
         """
-        # Load the data
-        self.data = data
+        # Load the data 
+        self.load_data(data_path)
         
         # Set the loss function
         if loss_function == 'r2':
             self.loss_function = self.r2
         elif loss_function == 'rmse':
             self.loss_function = self.rmse
+        elif loss_function == 'combined':
+            self.loss_function = self.combined
         else:
             raise ValueError('Invalid loss function.')
         
-    def subset(self, features) -> pd.DataFrame:
+        # Set the regression model
+        if regression_model == 'ridge':
+            self.regression_model = Ridge (alpha=1.0, copy_X=True, fit_intercept=True, 
+                                           max_iter=None, random_state=None, 
+                                           solver='auto', tol=0.001)
+            
+        elif regression_model == 'lasso':
+            self.regression_model = Lasso(alpha=1.0, copy_X=True, fit_intercept=True, 
+                                          max_iter=1000, positive=False, 
+                                          random_state=None,selection='cyclic', 
+                                          tol=0.0001, warm_start=False)
+        
+        elif regression_model == 'linear':
+            self.regression_model = LinearRegression(copy_X=True, fit_intercept=True, 
+                                                     n_jobs=1)
+        
+        else:
+            raise ValueError('Invalid regression model.')
+        
+    def load_data(self, data_path):
         """
-        Get a subset of the data based on the features.
-        """ 
-        # Return the subset of the data
-        return self.data[features]
-    
-    def evaluate(self, features)
+        Load the data from the csv file.
+        """
+        #Load the csv
+        df = pd.read_csv(data_path)
+        
+        #Find the numeric columns
+        numeric_columns = df.select_dtypes(include=['int64', 'float64']).columns
+        
+        #Drop the non-numeric columns
+        df = df[numeric_columns]
+        
+        #Fill the missing values with 0
+        for column in df.columns:
+            df[column] = df[column].fillna(0)
+        
+        #Split the data
+        Y = df['price']
+        X = df.drop(columns=['price'])
+                
+        self.features = X.columns
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        
+    def evaluate(self, features):
         """
         Evaluate the loss function for a subset of features.
+        
+        Parameters
+        ----------
+        features : list
+            The subset of features to evaluate coded as a binary array.
         """
-        # Get the subset of the data
-        subset = self.subset(features)
-        y_true = subset['y']
-        y_pred = subset['y_pred']
-        # Evaluate the loss function
-        return self.loss_function(subset)
-    
+        #Turn features into a boolean arrat umbralized at 0.5
+        features = [feature > 0.5 for feature in features]
+        
+        #Get the selected features
+        features = [feature for feature, include in zip(self.features, features) if include]
+        X_train = self.x_train[features]
+        
+        #Fit the model
+        self.regression_model.fit(X_train, self.y_train)
+        
+        #Evaluate the model
+        X_test = self.x_test[features]
+        y_pred = self.regression_model.predict(X_test)
+        
+        return self.loss_function(self.y_test, y_pred)
+            
     
     # Loss Functions
-    def r2(y_true, y_pred):
+    def r2(sefl, y_true, y_pred):
         """
         R2 Loss Function
         """
         return 1 - ((y_true - y_pred)**2).sum() / ((y_true - y_true.mean())**2).sum()
     
-    def rmse(y_true, y_pred):
+    def rmse(self, y_true, y_pred):
         """
         RMSE Loss Function
         """
         return ((y_true - y_pred)**2).mean()**0.5
+    
+    def combined(self, y_true, y_pred):
+        """
+        Linear combination of R2 and RMSE
+        """
+        return self.r2(y_true, y_pred) + 1/self.rmse(y_true, y_pred)
+    
+# %%
+ev = Evaluator(f'{ROOT_DIR}/data/listings.csv', 'ridge', 'rmse')
--- pso.py
+++ pso.py
@@ -1,3 +1,4 @@
+#%%
 """
 Particle Swarm Optimization Module
 ====================
@@ -6,17 +7,51 @@ This module contains the particle swarm optimization algorithm classes for the F
 
 import numpy as np
 import random
+from LossFunction import Evaluator
 
 class Particle:
+    """
+    Particle Class
+    """
     def __init__(self, dimension, position_min, position_max):
+        """
+        Constructor for the Particle class. Initilizes a particle with a random position and zero velocity.
+        
+        Parameters
+        ----------
+        dimension : int
+            The dimension of the particle (lenght).
+        position_min : float
+            The minimum value for the position.
+        position_max : float
+            The maximum value for the position.
+        """
         self.position = np.array([random.uniform(position_min, position_max) for _ in range(dimension)])
         self.velocity = np.zeros(dimension)
+        
+        # Initialize the best position and fitness as the initial position and fitness
         self.best_position = self.position.copy()
         self.best_fitness = float('inf')
 
     def update_velocity(self, global_best_position, w, c1, c2):
+        """
+        Update the velocity of the particle.
+        
+        Parameters
+        ----------
+        global_best_position : numpy.ndarray
+            The best position of the swarm.
+        w : float
+            Inertia weight.
+        c1 : float
+            Cognitive weight.
+        c2 : float
+            Social weight.
+        """
         r1 = random.uniform(0, 1)
         r2 = random.uniform(0, 1)
+        
+        # Update the velocity
         self.velocity = (
             w * self.velocity
             + c1 * r1 * (self.best_position - self.position)
@@ -24,29 +59,73 @@ class Particle:
         )
 
     def update_position(self, position_min, position_max):
+        """
+        Update the position of the particle. Clip the position to the position_min and position_max values.
+        
+        Parameters
+        ----------
+        position_min : float
+            The minimum value for the position.
+        position_max : float
+            The maximum value for the position.
+        """
         self.position += self.velocity
         self.position = np.clip(self.position, position_min, position_max)
 
 
 class PSO:
-    def __init__(self, population_size, dimension, position_min, position_max, w, c1, c2, fitness_function):
+    """
+    PSO Class
+    """
+    def __init__(self, evaluator: Evaluator):
+        """
+        Constructor for the PSO class.
+        
+        Parameters
+        ----------
+        evaluator : Evaluator
+            The evaluator object for the FSP problem    
+        """
+        # Set for the FSP problem
+        self.dimension = 9
+        self.position_min = 0
+        self.position_max = 1
+        self.evaluator = evaluator
+    
+    def minimize(self, generations, population_size, w, c1, c2, fitness_criterion):
+        """
+        Function to minimize the loss function using the PSO algorithm.
+        
+        Parameters
+        ----------
+        generations : int
+            The number of generations to run the algorithm.
+        population_size : int
+            The number of particles in the swarm.
+        w : float
+            Inertia weight.
+        c1 : float
+            Cognitive weight.
+        c2 : float
+            Social weight.
+        fitness_criterion : float
+            The fitness value to reach to stop the algorithm.
+        """
+        # Hyperparameters
         self.population_size = population_size
-        self.dimension = dimension
-        self.position_min = position_min
-        self.position_max = position_max
         self.w = w
         self.c1 = c1
         self.c2 = c2
-        self.fitness_function = fitness_function
-        self.particles = [Particle(dimension, position_min, position_max) for _ in range(population_size)]
+        
+        # Initialize the particles
+        self.particles = [Particle(self.dimension, self.position_min, self.position_max) for _ in range(population_size)]
         self.global_best_position = None
         self.global_best_fitness = float('inf')
         self.fitness_history = []
-
-    def optimize(self, generations, fitness_criterion):
+        
         for t in range(generations):
             for particle in self.particles:
-                fitness = self.fitness_function(particle.position)
+                fitness = self.evaluator.evaluate(particle.position)
                 if fitness < particle.best_fitness:
                     particle.best_position = particle.position.copy()
                     particle.best_fitness = fitness
@@ -67,6 +146,43 @@ class PSO:
         self.print_results(t)
 
         return self.global_best_fitness, self.fitness_history
+    
+    def maximize(self, generations, population_size, w, c1, c2, fitness_criterion):
+        # Hyperparameters
+        self.population_size = population_size
+        self.w = w
+        self.c1 = c1
+        self.c2 = c2
+        
+        # Initialize the particles
+        self.particles = [Particle(self.dimension, self.position_min, self.position_max) for _ in range(population_size)]
+        self.global_best_position = None
+        self.global_best_fitness = float('-inf')
+        self.fitness_history = []
+        
+        for t in range(generations):
+            for particle in self.particles:
+                fitness = self.evaluator.evaluate(particle.position)
+                if fitness > particle.best_fitness:
+                    particle.best_position = particle.position.copy()
+                    particle.best_fitness = fitness
+
+                if fitness > self.global_best_fitness:
+                    self.global_best_position = particle.position.copy()
+                    self.global_best_fitness = fitness
+
+            if np.mean([particle.best_fitness for particle in self.particles]) >= fitness_criterion:
+                break
+
+            for particle in self.particles:
+                particle.update_velocity(self.global_best_position, self.w, self.c1, self.c2)
+                particle.update_position(self.position_min, self.position_max)
+
+            self.fitness_history.append(self.global_best_fitness)
+
+        self.print_results(t)
+
+        return self.global_best_fitness, self.fitness_history
 
     def print_results(self, generations):
         print(f'Initial setup: w={self.w}, c1={self.c1}, c2={self.c2}')
@@ -75,3 +191,19 @@ class PSO:
         print('Average Particle Best Fitness Value: ', np.mean([particle.best_fitness for particle in self.particles]))
         print('Number of Generations: ', generations + 1)
         print('-' * 150)
+
+# %%
+evaluator = Evaluator('data/listings.csv', 'linear', 'rmse')
+evaluator.evaluate([1,1,1,1,1,1,1,1,1])
+
+#%%
+pso = PSO(evaluator)
+features, _ = pso.minimize(100, 100, 0.5, 1, 1, 0)
+features = pso.global_best_position
+features = [i>0.5 for i in features]
+all_features = evaluator.features
+selected_features = [feature for feature, include in zip(all_features, features) if include]
+print(f'Selected features: {selected_features}')
+
+# %%
+# %%
